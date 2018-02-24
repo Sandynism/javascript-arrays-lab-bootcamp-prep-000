9a8c60877d5b4a38b59aa2f7dc022720034459f2
Sandy Yeung <sandynism@gmail.com>
2018-02-24 21:06:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const app = "I don't do much."
 var kittens = ['Milo', 'Otis', 'Garfield']
 
 function destructivelyAppendKitten(name) {
-  return kittens.push(name)
+  return kittens.push(name) //adds kitten to end
 }
 
 function destructivelyPrependKitten(name) {
-  return kittens.unshift(name)
+  return kittens.unshift(name) //adds kitten to beginning
 }
+
