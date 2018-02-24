3890e577857179bd66015ec78700497228e65772
Sandy Yeung <sandynism@gmail.com>
2018-02-24 21:05:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const app = "I don't do much."
 
 var kittens = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendKitten(name) {
+  return kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  return kittens.shift(name)
+}
